55725822acb2bee54414073942ec01d4ee67ffde
Alex Amiryan <alex@amiryan.org>
2011-06-02 16:24:47 +0500

    Some small bugfixes

--- packages/Db/Memcache/DefaultConfig.inc.php
+++ packages/Db/Memcache/DefaultConfig.inc.php
@@ -2,7 +2,6 @@
 $defaultConfig = array(	'enabled' => true,
 						'host' => '127.0.0.1',
 						'port' => "11211",
-						'enabled' => false,
 						'keyPrefix' => "",
 						'Objects' => array("query" => "sql"),
 						'hooks' => array(  'BeforePluginInit' => 'AddMemcacheTimeConfig'  )
