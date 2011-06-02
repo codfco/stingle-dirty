d4eb92e1c41d586d95646b788af4e70fb7e73df2
Alex Amiryan <alex@amiryan.org>
2011-06-02 15:50:58 +0500

    Memcache upgrade to be able to keep per site global prefix and calling className
    Conflicts:
    
    	packages/Db/Memcache/DefaultConfig.inc.php

--- packages/Db/Memcache/DefaultConfig.inc.php
+++ packages/Db/Memcache/DefaultConfig.inc.php
@@ -3,6 +3,7 @@ $defaultConfig = array(	'enabled' => true,
 						'host' => '127.0.0.1',
 						'port' => "11211",
 						'enabled' => false,
+						'keyPrefix' => "",
 						'Objects' => array("query" => "sql"),
 						'hooks' => array(  'BeforePluginInit' => 'AddMemcacheTimeConfig'  )
 					);
