9833cbe1f0a6615db760f927375dbf9a71c9e4e8
Aram Gevorgyan <aram@web-emedianet.com>
2012-03-30 14:55:22 +0500

    Fix facebook photos albums limitation

--- packages/ExternalAuth/FacebookAuth/FacebookAuth.class.php
+++ packages/ExternalAuth/FacebookAuth/FacebookAuth.class.php
@@ -259,7 +259,7 @@ class FacebookAuth extends DbAccessor implements ExternalAuth
     	if($accessToken === null) {
     		$accessToken = $this->getAccessToken($code);
     	}
-    	$extAlbumUrl = "https://graph.facebook.com/me/albums?$accessToken";
+    	$extAlbumUrl = "https://graph.facebook.com/me/albums?$accessToken&limit=0";
  		$extAlbum = json_decode(file_get_contents($extAlbumUrl));
  		
  		foreach($extAlbum->data as $fbAlbum) {
