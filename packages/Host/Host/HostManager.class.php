d8ee378df2542ec8490cab800466d745103e9d2d
Alex Amiryan <alex@amiryan.org>
2016-12-14 16:43:47 +0400

    Fixed host manager strict standards

--- packages/Host/Host/HostManager.class.php
+++ packages/Host/Host/HostManager.class.php
@@ -12,7 +12,8 @@ class HostManager{
 		}
 		else{			
 			$originalHostName = $hostName;
-			$parentHostName = array_pop(explode(".", $hostName, 2));
+			$hostsArray = explode(".", $hostName, 2);
+			$parentHostName = array_pop($hostsArray);
 			$wildcardHostName = "*.". $parentHostName;
 			
 			$sql->exec("SELECT * FROM `".Tbl::get('TBL_HOSTS', 'Host')  ."` WHERE `host` = '{$wildcardHostName}'", $cacheMinutes);
