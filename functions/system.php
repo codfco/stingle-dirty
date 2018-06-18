f1de380205de86ed944591a0a6753d7f00e3b87e
Alex Amiryan <alex@amiryan.org>
2018-06-18 21:26:40 +0400

    Exception handling fix

--- functions/system.php
+++ functions/system.php
@@ -2,43 +2,20 @@
 function default_exception_handler(Exception $e){
 	HookManager::callHook('NoDebugExceptionHandler', array('e' => $e));
 	
-	$config = ConfigManager::getGlobalConfig();
 	if(Debug::getMode()){
 		echo format_exception($e, true);
 	}
 	else{
 		HookManager::callHook('ExceptionHandler', array('e' => $e));
 	}
-	if(Reg::get('packageMgr')->isPluginLoaded("Db","Db") and function_exists("write_log")){
-		@write_log("Exception", format_exception($e));
-	}
 	
-	if($config->Debug->send_email_on_exception and function_exists("send_mail")){
-		//@send_mail($config->site->developer_mail, "Exception", format_exception($e, true));
-	}
 	exit;
 }
 
-function default_error_handler($errno, $errstr, $errfile, $errline){
-	$config = ConfigManager::getGlobalConfig();
-	
-	if ( $errno === E_RECOVERABLE_ERROR or $errno === E_WARNING ) {
-		if(Debug::getMode()){
-			echo format_error($errno, $errstr, $errfile, $errline, true);
-		}
-		else{
-			HookManager::callHook('ErrorHandler', array('e' => $e));
-		}
-		if(Reg::get('packageMgr')->isPluginLoaded("Db","Db") and function_exists("write_log")){
-			@write_log("Eerror", format_error($errno, $errstr, $errfile, $errline));
-		}
-		
-		if($config->Debug->send_email_on_exception and function_exists("send_mail")){
-			//@send_mail($config->site->developer_mail, "Error", format_error($errno, $errstr, $errfile, $errline, true));
-		}
-		exit;
+function default_error_handler($severity, $message, $file, $line){
+	if ( $severity === E_RECOVERABLE_ERROR or $severity === E_WARNING ) {
+		throw new ErrorException($message, $severity, $severity, $file, $line);
 	}
-	return false;
 }
 
 function shutdown(){
